8d00686d3a64f1457060eee0f93e251d5329223f
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-18 17:36:24 +0530

    overriding method

---
+++ MethodOverriding/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverriding
+{
+    class Animal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    class Pig : Animal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myPig = new Pig();
+           
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+          
+        }
+
+    }
+}
