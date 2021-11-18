a511ac3f93ae63c6e407c32249c8700af9566540
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-18 09:04:36 +0530

    Creating Class and object

---
+++ ClassAndObjectConcepts/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPsConcepts.ClassAndObjectConcepts
+{
+   public class Car
+    {
+        //instance variable
+        public string color = "Red";
+    }
+}
---
+++ ClassAndObjectConcepts/Program.cs
@@ -0,0 +1,20 @@
+﻿using OOPsConcepts.ClassAndObjectConcepts;
+using System;
+
+namespace OOPsConcepts
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Car swift = new Car();
+            swift.color = "Black";
+            Console.WriteLine("swift color = " + swift.color);
+            Car maruthi = new Car();
+            maruthi.color = "pink";
+            Console.WriteLine("maruthi color = " + maruthi.color);
+            Console.ReadKey();
+        }
+
+    }
+}
