1d1423925c2170d6d0b5ebf6013f5b25f3fdbc9b
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-18 17:52:46 +0530

    OOPs Abstraction

---
+++ OOpsAbstraction/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOpsAbstraction
+{
+    abstract class Animal
+    {
+
+        public abstract void animalSound();
+
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+}
---
+++ OOpsAbstraction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOpsAbstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig(); 
+            myPig.animalSound();  
+            myPig.sleep(); 
+        }
+    }
+}
