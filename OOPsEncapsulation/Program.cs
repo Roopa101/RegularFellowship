09582712d0d6dc5de922af5c89f48242c639c3e2
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-18 18:02:19 +0530

    Encapsulation using get set

---
+++ OOPsEncapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOPsEncapsulation
+{
+   
+    class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+    class Details
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Id = "10";
+            employee.Name = "Roopa";
+            employee.City = "Kalaburagi";
+
+            Console.WriteLine("Employee Id: " + employee.Id);
+            Console.WriteLine("Employee Name: " + employee.Name);
+            Console.WriteLine("Employee City: " + employee.City);
+        }
+    }
+}
